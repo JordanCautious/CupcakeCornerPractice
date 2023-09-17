fae7a982e6d82899b9e9b182d0b584b650604f63
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-09-17 04:04:49 -0400

    Initial Commit

---
+++ CupcakeCornerPractice/CupcakeCornerPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerPracticeApp.swift
+//  CupcakeCornerPractice
+//
+//  Created by Jordan Haynes on 9/17/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
